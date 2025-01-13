d0266cdf83d488ab8e3b3c9088a8d1b511e2aa8c
Regmi-kz <regmi.main@gmail.com>
2025-01-13 11:09:24 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let burger = document.querySelector(".header__burger");
+    burger.addEventListener("click", function (event) {
+        burger.classList.toggle("active");
+        document.querySelector(".header__menu").classList.toggle("active");
+        document.querySelector("body").classList.toggle("lock");
+    });
+});
